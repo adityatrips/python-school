bc049f6111f1492170293b3b6980e75c243863b9
Aditya Tripathi <axityatrips@gmail.com>
2020-07-22 19:57:39 +0530

    Completed  Q30

--- 3 - Flow of Control/30.py
+++ 3 - Flow of Control/30.py
@@ -6,4 +6,18 @@ def isPrime(n):
     return True
 
 
+def prime_fact(n):
+  fact = []
+  for i in range(2, n//2):
+    if (n % i) == 0:
+      fact.append(i)
+    else:
+      pass
+  return fact
+
+
 n = int(input("ENTER A NUMBER\n=> "))
+prime_f = prime_fact(n)
+for number in prime_f:
+  if isPrime(number):
+    print(number, end=' ')
